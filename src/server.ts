9719196695871ceb7ebabc4eb621db7c73d8db2f
Rohit Tewari <rtewari056@gmail.com>
2023-09-14 15:06:35 +0530

    Add MySQL connector in server

--- src/server.ts
+++ src/server.ts
@@ -2,21 +2,32 @@ import express, { Express, Request, Response } from 'express';
 import cors from 'cors'
 import dotenv from 'dotenv';
 import path from 'path';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
 
-dotenv.config({ path: path.resolve(process.cwd(), 'src/.env.local') });
+// Database connection
+import connection from "./config/db.config.js";
 
-const app: Express = express();
-app.use(express.json()); // Accept JSON data
+// Config environment variables
+dotenv.config({ path: path.resolve(process.cwd(), 'src/config/.env.local') });
+
+const server: Express = express();
+server.use(express.json()); // Accept JSON data
 const PORT: string | undefined = process.env.PORT || '5000';
 
-app.use(cors({
-    credentials: true
-}))
+server.use(cors({ credentials: true })); // Enable CORS
+server.use(compression()); // Compress responses
+server.use(cookieParser()); // Parse cookies
 
-app.get('/', (req: Request, res: Response) => {
+server.get('/', (req: Request, res: Response) => {
     res.send('<h1>HELLO FROM Express + TypeScript</h1>')
 });
 
-app.listen(PORT, () => {
+server.get('/api/data', async (req: Request, res: Response) => {
+    const [data] = await connection.query("SELECT * FROM test.User");
+    res.json(data)
+});
+
+server.listen(PORT, () => {
     console.log(`⚡️ Server is running at http://localhost:${PORT}`);
 })
