7c8f77c85ef37f890a166008694784bf2a7a00ac
Rohit Tewari <rtewari056@gmail.com>
2024-01-29 01:05:40 +0530

    Fix auth import statement

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,7 @@
 import express, { Router } from 'express';
 
 import user from '../controllers/user.controller.js';
-import auth from '../middlewares/index.js'
+import auth from '../middlewares/isAuthenticated.middleware.js'
 
 const router: Router = express.Router();
 
